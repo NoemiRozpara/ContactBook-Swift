e8f7af9b4b5bd2da07a9f68d6ddb54f6d61cab4d
Noemi Rozpara <nrozpara@gmail.com>
2020-08-03 15:42:53 +0200

    Initial Commit

---
+++ ContactBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactBook
+//
+//  Created by Noemi Rozpara on 03/08/2020.
+//  Copyright © 2020 Noemi Rozpara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
